22f1044d859ce3ef0da601827174631a48418815
hovananhkim <hvakim.vn@gmail.com>
2021-06-10 19:21:15 +0700

    Fix exercise 9

--- src/main/java/Exercise09.java
+++ src/main/java/Exercise09.java
@@ -1,14 +1,10 @@
 public class Exercise09 {
-    public double dau(int index){
-        if (index%2==0) return -1;
-        else return 1;
-    }
     public double Pi(int n){
         double Pi=3;
         for (int i=1; i<=n;i++){
 
-            Pi+=dau(i)*4/(2*i*(2*i+1)*(2*i+2));
+            Pi+=Math.pow(-1,+i+1)*4/(2*i*(2*i+1)*(2*i+2));
         }
-        return Pi;
+        return Math.round(Pi*100000)/100000.0;
     }
 }
