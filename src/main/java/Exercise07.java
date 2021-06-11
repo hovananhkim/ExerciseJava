1fcfb93a5b5b403adfe17256a441785e8da2f05c
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-11 12:35:20 +0700

    Remove files

--- src/main/java/Exercise07.java
+++
@@ -1,6 +0,0 @@
-public class Exercise07 {
-    public int Factorial(int n){
-        if (n==0) return 1;
-        return Factorial(n-1)*n;
-    }
-}
--- src/test/java/Exercise17Test.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class Exercise17Test {
-    @Test
-    public void test_Sqrt(){
-        assertEquals(new Exercise17().Sqrt(9),3);
-    }
-}
