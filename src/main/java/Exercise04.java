81341f42b67e778ca44e326a2ab79bab11128991
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-11 12:39:37 +0700

    Added exercise 03

---
+++ src/main/java/Exercise04.java
@@ -0,0 +1,16 @@
+public class Exercise04 {
+    public String listFibonacy(int n){
+        String listFibonacy="";
+        if (n>0){
+            listFibonacy+="1 1";
+            int x=1,y=1,z;
+            while (x+y<=n){
+                z=x+y;
+                x=y;
+                y=z;
+                listFibonacy+=" "+z;
+            }
+        }
+        return listFibonacy;
+    }
+}
