0edb9910a1d1992c5f589c826853f048953c8a3f
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-11 12:42:49 +0700

    Added exercise 14

---
+++ src/test/java/Exercise14Test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class Exercise14Test {
+    @Test
+    public void test_isSumxEponentiation(){
+        assertEquals(new Exercise14().isSumxEponentiation(153),3);
+    }
+}
