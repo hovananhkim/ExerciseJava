f96400bec4da2d2228990c9f71ac00bc3518bfbb
hovananhkim <hvakim.vn@gmail.com>
2021-06-15 21:27:58 +0700

    out

--- src/main/java/Exercise03.java
+++ src/main/java/Exercise03.java
@@ -1,16 +1,19 @@
 public class Exercise03 {
-    public String primeList(int n){
-        String primeList="";
-        int prime=2;
-        if (n==2) return "2";
-        while(n>=prime){
-            while(n%prime==0){
-                primeList+=prime+"*";
-                n/=prime;
+    public String primeList(int n) {
+        String primeList = String.join("");
+        int prime = 2;
+        if (n == 2) {
+            return "2";
+        }
+        while (n >= prime ) {
+            while (n % prime == 0) {
+//                primeList += prime + "*";
+                String.join(primeList, Integer.toString(prime), "*");
+                n /= prime;
             }
             prime++;
         }
-
-        return primeList.substring(0,primeList.length()-1);
+        System.out.println(primeList);
+        return primeList.substring(0, primeList.length() - 1);
     }
 }
