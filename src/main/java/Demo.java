ab8eef06b40175921e79138b4375c0976bd576e1
hovananhkim <hvakim.vn@gmail.com>
2021-06-15 21:07:35 +0700

    Format code Exercise02

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -11,3 +11,4 @@ public class Demo {
         return x*y;
     }
 }
+
--- src/main/java/Exercise02.java
+++ src/main/java/Exercise02.java
@@ -1,6 +1,8 @@
 public class Exercise02 {
     public int digSum(int n){
-        if (n < 10) return n;
+        if (n < 10) {
+            return n;
+        }
         return digSum(n/10)+n%10;
     }
 }
