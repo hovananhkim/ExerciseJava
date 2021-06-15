a1b33ff67ff9b30b1f2f552ccb698d3dc378ff74
hovananhkim <hvakim.vn@gmail.com>
2021-06-15 21:35:29 +0700

    Use String.format and format code

--- src/test/java/Exercise05Test.java
+++ src/test/java/Exercise05Test.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise05Test {
     @Test
     public void test_solveQuadraticEquation(){
-        assertEquals(new Exercise05().solveQuadraticEquation(1,-4,3),"3.0 1.0");
-        assertEquals(new Exercise05().solveQuadraticEquation(1,-2,1),"1.0");
+        assertEquals(new Exercise05().solveQuadraticEquation(1,-4,3),"3.0000 1.0000");
+        assertEquals(new Exercise05().solveQuadraticEquation(1,-2,1),"1.0000");
         assertEquals(new Exercise05().solveQuadraticEquation(1,-2,5),"complex");
 
     }
