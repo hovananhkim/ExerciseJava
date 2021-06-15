69f595084a1134fc99c6e89843e2cf66b1172ed2
hovananhkim <hvakim.vn@gmail.com>
2021-06-15 21:04:26 +0700

    Fix and format code Exercise04

--- src/main/java/Exercise04.java
+++ src/main/java/Exercise04.java
@@ -1,14 +1,14 @@
 public class Exercise04 {
-    public String listFibonacy(int n){
-        String listFibonacy="";
-        if (n>0){
-            listFibonacy+="1 1";
-            int x=1,y=1,z;
-            while (x+y<=n){
-                z=x+y;
-                x=y;
-                y=z;
-                listFibonacy+=" "+z;
+    public String listFibonacy(int n) {
+        String listFibonacy = "";
+        if (n > 0) {
+            listFibonacy.join("1 1");
+            int x = 1, y = 1, z;
+            while (x + y <= n) {
+                z = x + y;
+                x = y;
+                y = z;
+                listFibonacy.join(" ", Integer.toString(z));
             }
         }
         return listFibonacy;
