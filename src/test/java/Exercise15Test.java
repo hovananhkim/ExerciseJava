f3b13ee3d81dd90f721f12a7b6be82e59f243078
hovananhkim <hvakim.vn@gmail.com>
2021-06-16 09:46:52 +0700

    Format code Exercise15

--- src/test/java/Exercise15Test.java
+++ src/test/java/Exercise15Test.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Exercise15Test {
     @Test
-    public void test_binaryReserve(){
-        assertEquals(new Exercise15().binaryReverse(8),1);
-        assertEquals(new Exercise15().binaryReverse(23),29);
+    public void test_binaryReserve() {
+        assertEquals(new Exercise15().binaryReverse(8), 1);
+        assertEquals(new Exercise15().binaryReverse(23), 29);
     }
 }
