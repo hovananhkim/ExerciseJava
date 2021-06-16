44a33a0cf1141c238a9fe52783f45ecf8954998e
hovananhkim <hvakim.vn@gmail.com>
2021-06-16 10:03:06 +0700

    Fix Exercise03

--- src/main/java/Exercise03.java
+++ src/main/java/Exercise03.java
@@ -1,19 +1,18 @@
 public class Exercise03 {
     public String primeList(int n) {
-        String primeList = String.join("");
         int prime = 2;
+        String primeList = "";
         if (n == 2) {
             return "2";
         }
-        while (n >= prime ) {
+        while (n >= prime) {
             while (n % prime == 0) {
-//                primeList += prime + "*";
-                String.join(primeList, Integer.toString(prime), "*");
+                primeList = String.join("*", primeList, Integer.toString(prime));
                 n /= prime;
             }
             prime++;
         }
         System.out.println(primeList);
-        return primeList.substring(0, primeList.length() - 1);
+        return primeList.substring(1);
     }
 }
