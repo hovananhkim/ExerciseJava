7456dbb91b83d2ce2c39eb6e50ed77fba39b5088
hovananhkim <hvakim.vn@gmail.com>
2021-06-16 10:05:37 +0700

    Use String.join

--- src/main/java/Exercise04.java
+++ src/main/java/Exercise04.java
@@ -2,13 +2,13 @@ public class Exercise04 {
     public String listFibonacy(int n) {
         String listFibonacy = "";
         if (n > 0) {
-            listFibonacy.join("1 1");
+            listFibonacy = "1 1";
             int x = 1, y = 1, z;
             while (x + y <= n) {
                 z = x + y;
                 x = y;
                 y = z;
-                listFibonacy.join(" ", Integer.toString(z));
+                listFibonacy = listFibonacy.join(" ", listFibonacy, Integer.toString(z));
             }
         }
         return listFibonacy;
