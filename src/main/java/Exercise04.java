59119a4bac0503c767dae11d453e408a218aeaa4
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 10:34:41 +0700

    Fix codecov

--- src/main/java/Exercise04.java
+++ src/main/java/Exercise04.java
@@ -1,15 +1,13 @@
 public class Exercise04 {
     public String listFibonacy(int n) {
         String listFibonacy = "";
-        if (n > 0) {
-            listFibonacy = "1 1";
-            int x = 1, y = 1, z;
-            while (x + y <= n) {
-                z = x + y;
-                x = y;
-                y = z;
-                listFibonacy = listFibonacy.join(" ", listFibonacy, Integer.toString(z));
-            }
+        listFibonacy = "1 1";
+        int x = 1, y = 1, z;
+        while (x + y <= n) {
+            z = x + y;
+            x = y;
+            y = z;
+            listFibonacy = listFibonacy.join(" ", listFibonacy, Integer.toString(z));
         }
         return listFibonacy;
     }
--- src/test/java/Exercise04Test.java
+++ src/test/java/Exercise04Test.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Exercise04Test {
     @Test
-    public void test_listFibonacy(){
-        assertEquals(new Exercise04().listFibonacy(8),"1 1 2 3 5 8");
+    public void test_listFibonacy() {
+        assertEquals(new Exercise04().listFibonacy(8), "1 1 2 3 5 8");
     }
 }
