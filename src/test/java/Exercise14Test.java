90d6dda3c566df61323a84c857904fd704bf9d53
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 10:37:35 +0700

    Add Exercise10

--- src/test/java/Exercise14Test.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class Exercise14Test {
-    @Test
-    public void test_isSumxEponentiation(){
-        assertEquals(new Exercise14().isSumEponentiation(153),3);
-    }
-}
