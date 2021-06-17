efbc7042252f38431a72a78496b5f88677c79424
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 16:41:16 +0700

    Issues

---
+++ src/main/java/String07.java
@@ -0,0 +1,12 @@
+public class String07 {
+    public String decodeShortcutString(String strShortcut) {
+        int [] listNum = new int[strShortcut.length()];
+        for (int i=0;i<strShortcut.length();i++){
+            if (strShortcut.charAt(i)>='A'){
+                listNum[i]
+            }
+        }
+        System.out.println((int)strShortcut.charAt(1));
+        return strShortcut;
+    }
+}
---
+++ src/test/java/String07Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class String07Test {
+    @Test
+    public void test_decodeShortcutString(){
+        new String07().decodeShortcutString("");
+//        new String07().deleteNumber("ad12c6d");
+    }
+}
