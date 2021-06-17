127a2441f718a59cdc24ff928509b48ffc7bb5a9
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 11:13:20 +0700

    Fix sai so

--- src/main/java/Exercise17.java
+++ src/main/java/Exercise17.java
@@ -4,10 +4,11 @@ public class Exercise17 {
         double left = 0, right = n;
         double result = Math.sqrt(n);
         while (result - left >= 0.0001) {
-            double temp = (left + right) / 2;
+            double temp = (left + right) / 2.0;
             if (temp * temp >= n) right = temp;
             else left = temp;
         }
-        return Double.parseDouble(String.format("%.4f", left));
+//        return Double.parseDouble(String.format("%.4f", left));
+        return left;
     }
 }
