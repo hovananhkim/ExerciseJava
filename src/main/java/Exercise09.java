82afe786e12b1aaf90ff8db8bc6bbc500b81a16f
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 11:18:37 +0700

    Fix sai so

--- src/main/java/Exercise09.java
+++ src/main/java/Exercise09.java
@@ -2,10 +2,10 @@ public class Exercise09 {
     public double Pi() {
         double Pi = 3;
         double result = Math.PI;
-        for (int i = 1; Math.abs(result - Pi) > 0.00001; i++) {
+        for (int i = 1; Math.abs(result - Pi) >= 0.0001; i++) {
 
             Pi += Math.pow(-1, +i + 1) * 4 / (2 * i * (2 * i + 1) * (2 * i + 2));
         }
-        return Double.parseDouble(String.format("%.5f", Pi));
+        return Pi;
     }
 }
--- src/test/java/Exercise09Test.java
+++ src/test/java/Exercise09Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise09Test {
     @Test
     public void test_Pi() {
-        assertEquals(new Exercise09().Pi(), 3.1416);
+        assertEquals(new Exercise09().Pi(), 3.1416,0.0001);
     }
 }
