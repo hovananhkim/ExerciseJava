870687282231ffcd0a98d33a7724710c9d6ae5f4
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 08:42:02 +0700

    Add test case

--- src/test/java/Array15Test.java
+++ src/test/java/Array15Test.java
@@ -9,6 +9,9 @@ public class Array15Test {
     public void test() {
         int[] input = {1, 6, -3, 9, 2};
         int[] output = {2, 6, 9};
+        int[] input2 = {3, 1, 6, 9, 3};
+        int[] output2 = {3, 9, 6};
         assertEquals(Arrays.equals(new Array15().findMaxArray(input, 3), output), true);
+        assertEquals(Arrays.equals(new Array15().findMaxArray(input2, 3), output2), true);
     }
 }
