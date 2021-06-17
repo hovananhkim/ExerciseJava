c2f4ef3ecea6ebd6275784d652293867a1295627
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 15:11:22 +0700

    Fix Wrong problem

--- src/main/java/String06.java
+++ src/main/java/String06.java
@@ -1,10 +1,22 @@
 public class String06 {
     public String shortcutString(String str) {
-        for (int i = 0; i < str.length()-1; i++) {
-            if (str.charAt(i)==str.charAt(i+1)){
-                str = deleteCharacter(str,i);
-                i--;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                int count = 2;
+                str = deleteCharacter(str, i);
+                for (int j = i + 1; j < str.length() - 1; j--) {
+                    if (str.charAt(i) == str.charAt(j)) {
+                        str = deleteCharacter(str, j);
+                        j--;
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+                str = deleteCharacter(str, i);
+                str = String.join(Integer.toString(count), str.substring(0, i), str.substring(i));
             }
+
         }
         return str;
     }
--- src/test/java/String06Test.java
+++ src/test/java/String06Test.java
@@ -4,6 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class String06Test {
     @Test
     public void test_shortcutString(){
-        assertEquals(new String06().shortcutString("abbcccd"),"abcd");
+        assertEquals(new String06().shortcutString("abbcccd"),"a23d");
     }
 }
