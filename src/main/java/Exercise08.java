1b2fa15576a62c7fa540f34698afb35b04018209
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 11:28:36 +0700

    pi not Pi

--- src/main/java/Exercise08.java
+++ src/main/java/Exercise08.java
@@ -1,5 +1,5 @@
 public class Exercise08 {
-    public double Pi() {
+    public double pi() {
         float Pi = 0;
         double result = Math.PI;
         for (int i = 0; Math.abs(result - Pi) >= 0.00001; i++) {
