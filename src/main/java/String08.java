dc6cf810b025609183d6929b0c54b49d990d4d21
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 17:16:33 +0700

    Wrong problem

--- src/main/java/String08.java
+++ src/main/java/String08.java
@@ -4,15 +4,14 @@ public class String08 {
         int sumEven = 0;
         for (int i = 0; i < str.length(); i++) {
             if (i % 2 == 0) {
-                sumOdd += str.charAt(i);
+                sumOdd += Character.getNumericValue(str.charAt(i));
             } else {
-                sumEven += str.charAt(i);
+                sumEven += Character.getNumericValue(str.charAt(i));
             }
         }
-        if ((sumOdd+3*sumEven)%10==0){
+        if ((sumOdd + 3 * sumEven) % 10 == 0) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
