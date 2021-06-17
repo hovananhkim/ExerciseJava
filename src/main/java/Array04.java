c51fb84de36a39ee5a98ce00dbd6e5a98d0c88b0
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 22:01:40 +0700

    Fix return value

--- src/main/java/Array04.java
+++ src/main/java/Array04.java
@@ -1,12 +1,12 @@
 public class Array04 {
     public int[] array04(int[] array) {
         int len = array.length;
-        array = sort(array);
+        sort(array);
         int count = len - 1;
         int countEvenNumber = countEvenNumber(array);
         for (int i = 0; count>=countEvenNumber ; i++) {
             if (array[i] % 2 != 0) {
-                array = addpend(array, i, count);
+                addpend(array, i, count);
                 i--;
                 count--;
             }
@@ -24,7 +24,7 @@ public class Array04 {
         return count;
     }
 
-    public int[] sort(int[] array) {
+    public void sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = array.length - 1; j > i; j--) {
                 if (array[i] > array[j]) {
@@ -32,22 +32,19 @@ public class Array04 {
                 }
             }
         }
-        return array;
     }
 
-    public int[] addpend(int[] array, int x, int y) {
+    public void addpend(int[] array, int x, int y) {
         int temp = array[x];
         for (int i = x; i < y; i++) {
             array[i] = array[i + 1];
         }
         array[y] = temp;
-        return array;
     }
 
-    public int[] permutation(int[] array, int x, int y) {
+    public void permutation(int[] array, int x, int y) {
         int temp = array[x];
         array[x] = array[y];
         array[y] = temp;
-        return array;
     }
 }
