6112d25736b5fd5347b17fc04af7e3317afa2f21
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 11:02:56 +0700

    Fix sai so

--- src/main/java/Exercise09.java
+++ src/main/java/Exercise09.java
@@ -1,10 +1,11 @@
 public class Exercise09 {
-    public double Pi(int n){
-        double Pi=3;
-        for (int i=1; i<=n;i++){
+    public double Pi() {
+        double Pi = 3;
+        double result = Math.PI;
+        for (int i = 1; Math.abs(result - Pi) > 0.00001; i++) {
 
-            Pi+=Math.pow(-1,+i+1)*4/(2*i*(2*i+1)*(2*i+2));
+            Pi += Math.pow(-1, +i + 1) * 4 / (2 * i * (2 * i + 1) * (2 * i + 2));
         }
-        return Math.round(Pi*100000)/100000.0;
+        return Double.parseDouble(String.format("%.5f", Pi));
     }
 }
