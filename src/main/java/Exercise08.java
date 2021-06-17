2b6befea33fd272204863c4a7bb581ecb5441fb8
hovananhkim <hvakim.vn@gmail.com>
2021-06-17 11:21:07 +0700

    Fix sai so

--- src/main/java/Exercise08.java
+++ src/main/java/Exercise08.java
@@ -2,10 +2,9 @@ public class Exercise08 {
     public double Pi() {
         float Pi = 0;
         double result = Math.PI;
-        for (int i = 0; Math.abs(result - Pi) >= 0.0001; i++) {
+        for (int i = 0; Math.abs(result - Pi) >= 0.00001; i++) {
             Pi += Math.pow(-1, i) * 4 / (2 * i + 1);
         }
-        System.out.println(Pi);
-        return Double.parseDouble(String.format("%.4f", Pi));
+        return Pi;
     }
 }
