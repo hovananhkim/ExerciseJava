6e48e1216edfdd8faf1d30479728bfa950400816
hovananhkim <hvakim.vn@gmail.com>
2021-06-18 10:13:38 +0700

    Add test case

--- src/test/java/Exercise19Test.java
+++ src/test/java/Exercise19Test.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise19Test {
     @Test
     public void test_writeNumbertoRomanNumerals(){
-        assertEquals(new Exercise19().writeNumbertoRomanNumerals(1000),"M");
-        assertEquals(new Exercise19().writeNumbertoRomanNumerals(1234),"MCCXXXIV");
+        assertEquals(new Exercise19().writeNumbertoRomanNumerals(1999),"MCMXCIX");
+        assertEquals(new Exercise19().writeNumbertoRomanNumerals(555),"DLV");
+        assertEquals(new Exercise19().writeNumbertoRomanNumerals(444),"CDXLIV");
+        assertEquals(new Exercise19().writeNumbertoRomanNumerals(111),"CXI");
     }
 }
