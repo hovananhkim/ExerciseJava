c946822d6034122308da49b0fb08d63b06a19f12
hovananhkim <hvakim.vn@gmail.com>
2021-06-18 09:29:16 +0700

    Format code again

--- src/main/java/Exercise17.java
+++ src/main/java/Exercise17.java
@@ -1,13 +1,16 @@
 public class Exercise17 {
     public double sqrt(double n) {
-        //Do chinh xac 0.0001
         double left = 0;
         double right = n;
         double result = Math.sqrt(n);
         while (result - left >= 0.0001) {
             double temp = (left + right) / 2.0;
-            if (temp * temp >= n) right = temp;
-            else left = temp;
+            if (temp * temp >= n) {
+                right = temp;
+            }
+            else {
+                left = temp;
+            }
         }
         return left;
     }
