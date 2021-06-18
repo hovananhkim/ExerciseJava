ab077f70de33b8ec7b89c25950fdfd5f1698697f
hovananhkim <hvakim.vn@gmail.com>
2021-06-18 09:43:24 +0700

    Fix syntax

--- src/main/java/Exercise20.java
+++ src/main/java/Exercise20.java
@@ -1,17 +1,17 @@
 public class Exercise20 {
-    public int happyNumber(int n){
-        int loop = 100;
-        while (loop>0){
-            int temp=n;
-            int sum=0;
-            while (temp>0){
-                sum+=(temp%10)*(temp%10);
-                temp/=10;
+    public boolean happyNumber(int n) {
+        for (int loop = 100; loop > 0; loop--) {
+            int temp = n;
+            int sum = 0;
+            while (temp > 0) {
+                sum += (temp % 10) * (temp % 10);
+                temp /= 10;
             }
-            n=sum;
-            if (n==1) return n;
-            loop--;
+            n = sum;
+            if (n == 1) {
+                return true;
+            }
         }
-        return 0;
+        return false;
     }
 }
--- src/test/java/Exercise20Test.java
+++ src/test/java/Exercise20Test.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise20Test {
     @Test
     public void test_happyNumber(){
-        assertEquals(new Exercise20().happyNumber(19),1);
-        assertEquals(new Exercise20().happyNumber(10),1);
-        assertEquals(new Exercise20().happyNumber(17),0);
+        assertEquals(new Exercise20().happyNumber(19),true);
+        assertEquals(new Exercise20().happyNumber(10),true);
+        assertEquals(new Exercise20().happyNumber(17),false);
     }
 }
