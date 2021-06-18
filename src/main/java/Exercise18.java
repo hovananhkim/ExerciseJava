14924be4abe86670be94c9f68136e46e7a70547a
hovananhkim <hvakim.vn@gmail.com>
2021-06-18 10:00:41 +0700

    Change algorithm

--- src/main/java/Exercise18.java
+++ src/main/java/Exercise18.java
@@ -4,37 +4,23 @@ public class Exercise18 {
     public int readRomanNumerals(String romannumerals) {
         int len = romannumerals.length();
         int[] listValue = new int[len];
+        HashMap<Character, Integer> romanToInt = new HashMap<>();
+        romanToInt.put('I', 1);
+        romanToInt.put('V', 5);
+        romanToInt.put('X', 10);
+        romanToInt.put('L', 50);
+        romanToInt.put('C', 100);
+        romanToInt.put('D', 500);
+        romanToInt.put('M', 1000);
         for (int i = 0; i < len; i++) {
-            switch (romannumerals.charAt(i)) {
-                case 'I':
-                    listValue[i] = 1;
-                    break;
-                case 'V':
-                    listValue[i] = 5;
-                    break;
-                case 'X':
-                    listValue[i] = 10;
-                    break;
-                case 'L':
-                    listValue[i] = 50;
-                    break;
-                case 'C':
-                    listValue[i] = 100;
-                    break;
-                case 'D':
-                    listValue[i] = 500;
-                    break;
-                case 'M':
-                    listValue[i] = 1000;
-                    break;
-            }
+            listValue[i] = romanToInt.get(romannumerals.charAt(i));
         }
-        int result = listValue[len-1];
+        int result = listValue[len - 1];
         for (int i = len - 1; i > 0; i--) {
-            if (listValue[i]>listValue[i-1]){
+            if (listValue[i] > listValue[i - 1]) {
                 result -= listValue[i - 1];
-            }else {
-                result+=listValue[i-1];
+            } else {
+                result += listValue[i - 1];
             }
         }
         return result;
