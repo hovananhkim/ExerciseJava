6352fd32cf8bf0b14511b3ecdf3f519f8f659971
hovananhkim <hvakim.vn@gmail.com>
2021-06-18 08:53:06 +0700

    Fix syntax

--- src/main/java/Exercise05.java
+++ src/main/java/Exercise05.java
@@ -2,19 +2,15 @@ public class Exercise05 {
     public String solveQuadraticEquation(int a, int b, int c) {
         String result = "";
         double delta = b * b - 4 * a * c;
-        double root1;
-        double root2;
-
         if (delta < 0) {
-            result += "complex";
+            return "complex";
         } else if (delta == 0) {
             double root = (-b) / (2 * a);
-            result = String.format("%.4f", root);
+            return String.format("%.4f", root);
         } else {
-            root1 = (-b + Math.sqrt(delta)) / (2 * a);
-            root2 = (-b - Math.sqrt(delta)) / (2 * a);
-            result = String.format("%.4f %.4f", root1, root2);
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return String.format("%.4f %.4f", root1, root2);
         }
-        return result;
     }
 }
