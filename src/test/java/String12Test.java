f952e17eee3d6306bbd1a5d0dd07d7c678b192d9
hovananhkim <hvakim.vn@gmail.com>
2021-06-20 16:30:42 +0700

    Add testcase

--- src/test/java/String12Test.java
+++ src/test/java/String12Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class String12Test {
     @Test
     public void test_createMaxNumberFormArray() {
-        int[] input = {2, 3, 6, 21, 32, 34, 210};
-        assertEquals(new String12().createMaxNumberFromArray(input), "634332221210");
+        int[] input = {2, 3, 6, 21, 32, 34, 210, 61};
+        assertEquals(new String12().createMaxNumberFromArray(input), "66134332221210");
     }
 }
