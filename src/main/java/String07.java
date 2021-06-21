9d816839e6a430b3f06346c99deda474b924a5cd
hovananhkim <hvakim.vn@gmail.com>
2021-06-21 14:34:16 +0700

    Rename variable

--- src/main/java/String07.java
+++ src/main/java/String07.java
@@ -1,12 +1,12 @@
 public class String07 {
     public String decodeShortcutString(String str) {
         String characters = str.replaceAll("[0-9]", "");
-        String nums = str.replaceAll("[^0-9]", ",");
-        nums = nums.substring(1);
-        if (nums.charAt(nums.length() - 1) == ',') {
-            nums += "1";
+        String count = str.replaceAll("[^0-9]", ",");
+        count = count.substring(1);
+        if (count.charAt(count.length() - 1) == ',') {
+            count += "1";
         }
-        String[] listNum = nums.split(",");
+        String[] listNum = count.split(",");
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < listNum.length; i++) {
             if (listNum[i].equals("")) {
