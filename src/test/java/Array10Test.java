4b89273c1c6ba7e38cd15cc625e6d5ff0414e76a
hovananhkim <hvakim.vn@gmail.com>
2021-06-21 11:25:32 +0700

    Format code

--- src/test/java/Array10Test.java
+++ src/test/java/Array10Test.java
@@ -12,7 +12,6 @@ public class Array10Test {
         int[] output2 = {1, 2, 3, 3, 9, 12, 13};
         int[] output3 = {0, 1, 2, 3, 9, 12, 13};
         int[] output4 = {1, 2, 3, 9, 12, 13, 15};
-
         assertEquals(Arrays.equals(new Array10().addNumber(input, 5), output), true);
         assertEquals(Arrays.equals(new Array10().addNumber(input, 3), output2), true);
         assertEquals(Arrays.equals(new Array10().addNumber(input, 0), output3), true);
