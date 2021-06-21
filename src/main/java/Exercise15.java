545cc010e7cbdb0fd79b62438fa89bd193ba4825
hovananhkim <hvakim.vn@gmail.com>
2021-06-21 15:31:12 +0700

    Cach khac

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -1,13 +1,7 @@
 public class Exercise15 {
     public int binaryReverse(int n) {
-        int result = 0;
-        String binary = "";
-        while (n > 0) {
-            binary += n % 2;
-            n /= 2;
-        }
-        for (int i = 0; i < binary.length(); i++)
-            result += (binary.charAt(i) - '0') * Math.pow(2, binary.length() - 1 - i);
-        return result;
+        StringBuilder result = new StringBuilder(Integer.toBinaryString(n));
+        result = result.reverse();
+        return Integer.parseInt(result.toString(), 2);
     }
 }
