efc8766724ccd9a826eb59ed184d44f4981922c2
Võ Quang Hòa <voquanghoa@gmail.com>
2021-06-21 09:56:12 +0700

    Update String04Test.java

--- src/test/java/String04Test.java
+++ src/test/java/String04Test.java
@@ -4,7 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class String04Test {
     @Test
     public void test_isLoop(){
-        assertEquals(new String04().isLoop("abc"),false);
-        assertEquals(new String04().isLoop("ababc"),true);
+        assertEquals(new String04().isLoop("abc"), false);
+        assertEquals(new String04().isLoop("abab"), true);
+        assertEquals(new String04().isLoop("abcabc"), true);
+        assertEquals(new String04().isLoop("aaaaa"), true);
+        assertEquals(new String04().isLoop("abcdabcdabcd"), true);
+        assertEquals(new String04().isLoop("ababababab"), true);
+        assertEquals(new String04().isLoop("ababadbabab"), false);
+        assertEquals(new String04().isLoop("abababababc"), false);
     }
 }
