a3af7a2c7d5b521ac5d4ef38ab427ef8030cd00c
hovananhkim <hvakim.vn@gmail.com>
2021-06-21 14:12:24 +0700

    Use lastIndexOf

--- src/main/java/String05.java
+++ src/main/java/String05.java
@@ -1,29 +1,13 @@
-import java.util.HashMap;
-
 public class String05 {
     public String findCharacterAppearMoreTwice(String str) {
-        HashMap<Character, Integer> frequency = getFrequency(str);
         StringBuilder result = new StringBuilder();
-        for (Character key : frequency.keySet()) {
-            if (frequency.get(key) >= 2) {
-                result.append(key);
+        str = str.replaceAll(" ","");
+        while (str.length() > 1) {
+            if (str.lastIndexOf(str.charAt(0)) > 0) {
+                result.append(str.charAt(0));
             }
+            str = str.replaceAll(str.charAt(0) + "", "");
         }
-        return result.substring(0);
-    }
-
-    public HashMap<Character, Integer> getFrequency(String str) {
-        HashMap<Character, Integer> frequency = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' ') {
-                if (!frequency.containsKey(str.charAt(i))) {
-                    frequency.put(str.charAt(i), 0);
-                }
-                char key = str.charAt(i);
-                int value = frequency.get(key);
-                frequency.put(key, value + 1);
-            }
-        }
-        return frequency;
+        return result.toString();
     }
 }
--- src/test/java/String05Test.java
+++ src/test/java/String05Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class String05Test {
     @Test
     public void test_findCharacterAppearMoreTwice() {
-        assertEquals(new String05().findCharacterAppearMoreTwice("all c d c c a a d b"), "acdl");
+        assertEquals(new String05().findCharacterAppearMoreTwice("all c d c c a a d b"), "alcd");
     }
 }
