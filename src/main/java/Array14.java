2116fe273e3e0254bd6c8f1264a1c80efc53dd32
hovananhkim <hvakim.vn@gmail.com>
2021-06-21 17:27:00 +0700

    Tach ham

--- src/main/java/Array14.java
+++ src/main/java/Array14.java
@@ -1,27 +1,32 @@
 public class Array14 {
     public int determinant(int array[][], int n) {
-        int sum = 0;
+        int det = 0;
         if (n == 1) {
-            sum = array[0][0];
-        }
-        for (int k = 0; k < n; k++) {
-            int[][] temp = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 1; j < n; j++) {
-                    if (i < k) {
-                        temp[i][j - 1] = array[i][j];
-                    } else if (i > k) {
-                        temp[i - 1][j - 1] = array[i][j];
-                    }
+            det = array[0][0];
+        } else {
+            for (int k = 0; k < n; k++) {
+                int[][] temp = getArrayIgnoreRowAndColZero(array,k);
+                if (k % 2 == 0) {
+                    det += array[k][0] * determinant(temp, n - 1);
+                } else {
+                    det -= array[k][0] * determinant(temp, n - 1);
                 }
             }
-            if (k % 2 == 0) {
-                sum += array[k][0] * determinant(temp, n - 1);
-            } else {
-                sum -= array[k][0] * determinant(temp, n - 1);
-            }
-
         }
-        return sum;
+        return det;
+    }
+    public int[][] getArrayIgnoreRowAndColZero(int [][] array, int row){
+        int len = array.length;
+        int [][] result = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j < len; j++) {
+                if (i < row) {
+                    result[i][j-1] = array[i][j];
+                } else if (i > row) {
+                    result[i - 1][j-1] = array[i][j];
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/Array14Test.java
+++ src/test/java/Array14Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Array14Test {
     @Test
-    public void test() {
+    public void test_determinant() {
         int[][] input1 = {{1, 5}, {2, 6}};
         int[][] input2 = {{1, 5, 3}, {2, 6, 5}, {3, 4, 7}};
         int[][] input3 = {{1}};
