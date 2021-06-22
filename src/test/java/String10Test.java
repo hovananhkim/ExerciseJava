aaf5d8b373868efc102b34831fee1316eae88a61
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-22 10:02:46 +0700

    Add failed test case

--- src/test/java/String10Test.java
+++ src/test/java/String10Test.java
@@ -7,5 +7,7 @@ public class String10Test {
     public void test_sum() {
         assertEquals(new String10().sum("1234567895656666", "20222922"), "1234567915879588");
         assertEquals(new String10().sum("123", "4321"), "4444");
+        assertEquals(new String10().sum("999", "1"), "1000");
+        assertEquals(new String10().sum("999", "999"), "1998");
     }
 }
