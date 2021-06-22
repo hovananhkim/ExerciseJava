0129cf42f05426f0c3551e474ccf3b5f5f71db80
hovananhkim <hvakim.vn@gmail.com>
2021-06-22 20:32:24 +0700

    Fix

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -10,26 +10,25 @@ public class String10 {
         for (int i = a.length() - 1; i >= 0; i--) {
             int sum = a.charAt(i) + b.charAt(i) + memory - '0' * 2;
             if (sum < 10) {
-                result.append(sum);
+                result.insert(0, sum);
                 memory = 0;
             } else {
-                result.append(sum%10);
-                memory = sum/10;
+                result.append(sum % 10);
+                memory = sum / 10;
             }
         }
-        if (memory>0){
+        if (memory > 0) {
             result.append(memory);
         }
-        return result.reverse().toString();
+        return result.toString();
     }
 
     public String convertTwoStringEqualLength(String str, int len) {
         int lenStr = str.length();
         StringBuilder newStr = new StringBuilder(str);
-        newStr = newStr.reverse();
         for (int i = lenStr; i < len; i++) {
-            newStr = newStr.append(0);
+            newStr.insert(0, 0);
         }
-        return newStr.reverse().substring(0);
+        return newStr.toString();
     }
 }
