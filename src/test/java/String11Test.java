393845d6c6a2015960ecffe54e18fd580c136587
hovananhkim <hvakim.vn@gmail.com>
2021-06-22 21:20:01 +0700

    Fix

--- src/test/java/String11Test.java
+++ src/test/java/String11Test.java
@@ -6,6 +6,7 @@ public class String11Test {
     @Test
     public void test_sub() {
         assertEquals(new String11().sub("113", "23"), "90");
+        assertEquals(new String11().sub("52", "23"), "29");
         assertEquals(new String11().sub("11", "23"), "-12");
         assertEquals(new String11().sub("141332231", "99"), "141332132");
         assertEquals(new String11().sub("1", "1"), "0");
