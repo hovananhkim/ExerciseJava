8e892552a3f84b0d008cd1fb6c7f9a0da5498e6b
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-22 09:35:44 +0700

    Add failed test case

--- src/test/java/String11Test.java
+++ src/test/java/String11Test.java
@@ -7,5 +7,9 @@ public class String11Test {
     public void test_sub() {
         assertEquals(new String11().sub("113", "23"), "90");
         assertEquals(new String11().sub("141", "23"), "118");
+        assertEquals(new String11().sub("141332231", "99"), "141332132");
+        assertEquals(new String11().sub("1", "1"), "0");
+        assertEquals(new String11().sub("1", "10"), "-9");
+        assertEquals(new String11().sub("1", "100"), "-99");
     }
 }
