bb1f7bdcf31e897441dffc08d14dafcab69d5355
hovananhkim <hvakim.vn@gmail.com>
2021-06-22 21:09:21 +0700

    Fix

--- src/main/java/String11.java
+++ src/main/java/String11.java
@@ -1,23 +1,12 @@
 public class String11 {
-    public String sub(String max, String min) {
-        int len = max.length();
-        min = convertEqualLength(min, len);
-        StringBuilder result = new StringBuilder();
-        int sub = 0;
-        for (int i = len - 1; i >= 0; i--) {
-            if (max.charAt(i) - min.charAt(i) >= sub) {
-                result.append(max.charAt(i) - min.charAt(i) - sub);
-                sub = 0;
-            } else {
-                result.append(max.charAt(i) - min.charAt(i) - sub + 10);
-                sub = 1;
-            }
+    public String sub(String minuend, String subtrahend) {
+        if (minuend.equals(subtrahend)) {
+            return "0";
         }
-        result = result.reverse();
-        if (result.charAt(0) == '0') {
-            return result.substring(1);
+        if ((minuend.length() == subtrahend.length()&&minuend.compareTo(subtrahend)>0)||minuend.length() > subtrahend.length()) {
+            return subMaxMin(minuend, subtrahend).toString();
         }
-        return result.toString();
+        return subMaxMin(subtrahend, minuend).insert(0, "-").toString();
     }
 
     public String convertEqualLength(String str, int len) {
@@ -27,6 +16,27 @@ public class String11 {
         for (int i = lenStr; i < len; i++) {
             newStr = newStr.append(0);
         }
-        return newStr.reverse().substring(0);
+        return newStr.reverse().toString();
+    }
+
+    public StringBuilder subMaxMin(String minuend, String subtrahend) {
+        int lenMinuend = minuend.length();
+        subtrahend = convertEqualLength(subtrahend, lenMinuend);
+        StringBuilder result = new StringBuilder();
+        int memory = 0;
+        for (int i = lenMinuend - 1; i >= 0; i--) {
+            if (minuend.charAt(i) - subtrahend.charAt(i) >= memory) {
+                result.append(minuend.charAt(i) - subtrahend.charAt(i) - memory);
+                memory = 0;
+            } else {
+                result.append(minuend.charAt(i) - subtrahend.charAt(i) - memory + 10);
+                memory = 1;
+            }
+        }
+        result.reverse();
+        if (result.charAt(0) == '0') {
+            return result.delete(0, 1);
+        }
+        return result;
     }
 }
--- src/test/java/String11Test.java
+++ src/test/java/String11Test.java
@@ -6,7 +6,7 @@ public class String11Test {
     @Test
     public void test_sub() {
         assertEquals(new String11().sub("113", "23"), "90");
-        assertEquals(new String11().sub("141", "23"), "118");
+        assertEquals(new String11().sub("11", "23"), "-12");
         assertEquals(new String11().sub("141332231", "99"), "141332132");
         assertEquals(new String11().sub("1", "1"), "0");
         assertEquals(new String11().sub("1", "10"), "-9");
