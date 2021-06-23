e9912e6a4b35fb61ddeffda205019b5c9bc31a07
hovananhkim <hvakim.vn@gmail.com>
2021-06-24 01:05:12 +0700

    Fix

--- src/main/java/String08.java
+++ src/main/java/String08.java
@@ -1,15 +1,14 @@
 public class String08 {
     public boolean isBarCodeEAN13(String str) {
-        int sumOdd = 0;
-        int sumEven = 0;
+        int result = 0;
         for (int i = 0; i < str.length(); i++) {
             int num = str.charAt(i) - '0';
             if (i % 2 == 0) {
-                sumOdd += num;
+                result += num;
             } else {
-                sumEven += num;
+                result += num * 3;
             }
         }
-        return (sumOdd + 3 * sumEven) % 10 == 0;
+        return result % 10 == 0;
     }
 }
