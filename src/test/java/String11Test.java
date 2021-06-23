a077288a80259781a178cea85cf03fe9a61debed
hovananhkim <hvakim.vn@gmail.com>
2021-06-23 10:02:21 +0700

    Add testcase

--- src/test/java/String11Test.java
+++ src/test/java/String11Test.java
@@ -9,6 +9,7 @@ public class String11Test {
         assertEquals(new String11().sub("113", "23"), "90");
         assertEquals(new String11().sub("52", "23"), "29");
         assertEquals(new String11().sub("11", "23"), "-12");
+        assertEquals(new String11().sub("11", "11"), "0");
         assertEquals(new String11().sub("141332231", "99"), "141332132");
         assertEquals(new String11().sub("1", "100"), "-99");
     }
