5ecc8b39e22b9b8200b8fdea6151493566c9fe97
hovananhkim <hvakim.vn@gmail.com>
2021-06-24 00:59:59 +0700

    Fix

--- src/main/java/Array15.java
+++ src/main/java/Array15.java
@@ -1,26 +1,9 @@
+import java.util.Arrays;
+
 public class Array15 implements MaxArray {
     @Override
     public int[] findMaxArray(int[] array, int k) {
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = array[i];
-        }
-        for (int i = k; i < array.length; i++) {
-            int index = getIndexMinArray(result);
-            if (array[i] > result[index]) {
-                result[index] = array[i];
-            }
-        }
-        return result;
-    }
-
-    public int getIndexMinArray(int[] array) {
-        int minIndex= 0;
-        for (int j = 1; j < array.length; j++) {
-            if (array[minIndex] > array[j]) {
-                minIndex = j;
-            }
-        }
-        return minIndex;
+        Arrays.sort(array);
+        return Arrays.copyOfRange(array,array.length-k,array.length);
     }
 }
--- src/test/java/Array15Test.java
+++ src/test/java/Array15Test.java
@@ -1,17 +1,15 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Array15Test {
     @Test
     public void test() {
-        int[] input = {1, 6, -3, 9, 2};
-        int[] output = {2, 6, 9};
-        int[] input2 = {3, 1, 6, 9, 3};
-        int[] output2 = {3, 9, 6};
-        assertEquals(Arrays.equals(new Array15().findMaxArray(input, 3), output), true);
-        assertEquals(Arrays.equals(new Array15().findMaxArray(input2, 3), output2), true);
+        int[] input = {1, 6, -3, 9, 2, 9, 16};
+        int[] output = {2, 6, 9, 9, 16};
+        int[] input2 = {3, 1, 6, 9, 3, -7};
+        int[] output2 = {3, 6, 9};
+        assertArrayEquals(new Array15().findMaxArray(input, 5), output);
+        assertArrayEquals(new Array15().findMaxArray(input2, 3), output2);
     }
 }
