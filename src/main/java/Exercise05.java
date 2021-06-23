fdebd8aa5b144dcb798387b76f487cced66aa73b
hovananhkim <hvakim.vn@gmail.com>
2021-06-23 22:58:40 +0700

    Format

--- src/main/java/Exercise05.java
+++ src/main/java/Exercise05.java
@@ -3,13 +3,14 @@ public class Exercise05 {
         double delta = b * b - 4 * a * c;
         if (delta < 0) {
             return "complex";
-        } else if (delta == 0) {
+        }
+        if (delta == 0) {
             double root = (-b) / (2 * a);
             return String.format("%.4f", root);
-        } else {
-            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
-            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
-            return String.format("%.4f %.4f", root1, root2);
         }
+        double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+        return String.format("%.4f %.4f", root1, root2);
+
     }
 }
