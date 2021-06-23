308fa352e2cdd92b180b26ced5c7e5b56a4b871e
hovananhkim <hvakim.vn@gmail.com>
2021-06-24 00:46:29 +0700

    Fix array 13

--- src/test/java/Array13Test.java
+++ src/test/java/Array13Test.java
@@ -1,22 +1,25 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Array13Test {
     @Test
     public void test_deleteRowCol() {
-        int[][] input = {{1, 2, 3}, {3, 6, 7}, {3, 5, 7}};
-        int[][] output = {{1, 3}, {3, 7}};
-        int[][] output2 = {{2, 3}, {3, 7}};
-        int[][] output3 = {{2, 3}, {3, 7}, {4, 5}};
-        int[][] output4 = {{2, 3, 1}, {1, 3, 7}};
-        int[][] result = new Array13().deleteRowCol(input, 1, 1);
-        assertEquals(new Array13().isEquals(result, output), true);
-        assertEquals(new Array13().isEquals(result, output2), false);
-        assertEquals(new Array13().isEquals(result, output3), false);
-        assertEquals(new Array13().isEquals(result, output4), false);
-
+        int[][] input = {
+                {1, 2, 3},
+                {3, 6, 7},
+                {3, 5, 7}};
+        int[][] output = {
+                {1, 3},
+                {3, 7}};
+        int[][] output2 = {
+                {6, 7},
+                {5, 7}};
+        int[][] output3 = {
+                {1, 2},
+                {3, 6}};
+        assertArrayEquals(new Array13().deleteRowCol(input, 1, 1), output);
+        assertArrayEquals(new Array13().deleteRowCol(input, 0, 0), output2);
+        assertArrayEquals(new Array13().deleteRowCol(input, 2, 2), output3);
     }
 }
