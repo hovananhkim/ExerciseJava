f76dfde44ee40a6b2e89615d6225e161ddda6f71
hovananhkim <hvakim.vn@gmail.com>
2021-06-23 10:33:04 +0700

    Cach khac

--- src/main/java/Exercise10.java
+++ src/main/java/Exercise10.java
@@ -1,14 +1,16 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 public class Exercise10 {
     public String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-    public String getDayOfWeek(int day, int month, int year) {
-        int dayOfWeek = (day + ((153 * (month + 12 * ((14 - month) / 12) - 3) + 2) / 5) +
-                (365 * (year + 4800 - ((14 - month) / 12))) +
-                ((year + 4800 - ((14 - month) / 12)) / 4) -
-                ((year + 4800 - ((14 - month) / 12)) / 100) +
-                ((year + 4800 - ((14 - month) / 12)) / 400) - 32045) % 7;
-        return days[dayOfWeek];
+    public String getDayOfWeek(int day, int month, int year) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = String.format("%s/%s/%s", day, month, year);
+        Date date = dateFormat.parse(dateString);
+        return String.format("%tA",date);
     }
 }
