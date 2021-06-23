99bc9ac1815398d09e6e14b619e8873293d0b7ea
hovananhkim <hvakim.vn@gmail.com>
2021-06-23 22:26:28 +0700

    Fix

--- src/main/java/Exercise08.java
+++ src/main/java/Exercise08.java
@@ -1,10 +1,13 @@
 public class Exercise08 {
     public double pi() {
-        float Pi = 0;
-        double result = Math.PI;
-        for (int i = 0; Math.abs(result - Pi) >= 0.00001; i++) {
-            Pi += Math.pow(-1, i) * 4 / (2 * i + 1);
+        double pi = 0;
+        int n = 0;
+        double esp = 1;
+        while (esp > 0.0001) {
+            esp = 4.0 / (2 * n + 1);
+            pi += Math.pow(-1, n) * esp;
+            n++;
         }
-        return Pi;
+        return pi;
     }
 }
--- src/test/java/Exercise08Test.java
+++ src/test/java/Exercise08Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise08Test {
     @Test
     public void test_Pi() {
-        assertEquals(new Exercise08().pi(), 3.14159, 0.00001);
+        assertEquals(new Exercise08().pi(), 3.1416, 0.0001);
     }
 }
