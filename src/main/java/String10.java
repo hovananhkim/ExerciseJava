82f70b417088c7f74801167d3ee122002be52a4e
hovananhkim <hvakim.vn@gmail.com>
2021-06-24 09:57:16 +0700

    fix

--- src/main/java/String10.java
+++ src/main/java/String10.java
@@ -1,24 +1,35 @@
 public class String10 {
     public String sum(String a, String b) {
-        if (a.length() > b.length()) {
-            b = padZero(b, a.length());
-        } else {
-            a = padZero(a, b.length());
-        }
+
         StringBuilder result = new StringBuilder();
         int memory = 0;
-        for (int i = a.length() - 1; i >= 0; i--) {
-            int sum = a.charAt(i) + b.charAt(i) + memory - '0' * 2;
+        int length = a.length();
+        if (length < b.length()) {
+            length = b.length();
+        }
+        for (int i = 0; i < length; i++) {
+            int sum = getDigitFromRight(a, i) + getDigitFromRight(b, i) + memory;
             result.append(sum % 10);
             memory = sum / 10;
         }
         if (memory > 0) {
             result.append(memory);
         }
-        return result.reverse().toString();
+        return removeZeroPadding(result.reverse().toString());
     }
 
-    public String padZero(String str, int expectedLength) {
-        return String.format("%" + expectedLength + "s", str).replace(' ', '0');
+    private int getDigitFromRight(String str, int position) {
+        if (str.length() > position) {
+            return str.charAt(str.length() - position - 1) - '0';
+        }
+        return 0;
+    }
+
+    private String removeZeroPadding(String str) {
+        int foundIndex = 0;
+        while (foundIndex < str.length() - 1 && str.charAt(foundIndex) == '0') {
+            foundIndex++;
+        }
+        return str.substring(foundIndex);
     }
 }
