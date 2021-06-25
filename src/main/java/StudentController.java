1a5f6e44ef3474369f168dde4e80e95f44dc75fb
hovananhkim <hvakim.vn@gmail.com>
2021-06-25 09:58:02 +0700

    Use hashmap

--- src/main/java/StudentController.java
+++ src/main/java/StudentController.java
@@ -3,28 +3,29 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class StudentController {
-    private ArrayList<Student> students = new ArrayList<>();
+    private HashMap<Long, Student> map = new HashMap<>();
 
     public void add(Student student) {
-        students.add(student);
+        map.put(student.getId(), student);
     }
 
     public void delete(Student student) {
-        students.remove(student);
+        map.remove(student.getId(), student);
     }
 
-    public void deleteAt(int index) {
-        students.remove(index);
+    public void deleteAt(long index) {
+        map.remove(index);
     }
 
     public void saveToFile(String path) throws IOException {
-        String json = new Gson().toJson(students);
+        String json = new Gson().toJson(map);
         FileWriter fileWriter = new FileWriter(path);
         fileWriter.write(json);
         fileWriter.flush();
@@ -33,28 +34,27 @@ public class StudentController {
     public void loadFromFile(String path) throws FileNotFoundException {
         FileReader fileReader = new FileReader(path);
         String json = new Scanner(fileReader).useDelimiter("\\Z").next();
-        ArrayList<Student> students = new Gson().fromJson(json, new TypeToken<ArrayList<Student>>() {
+        HashMap<Long,Student> students = new Gson().fromJson(json, new TypeToken<HashMap<Long,Student>>() {
         }.getType());
-        this.students.clear();
-        this.students.addAll(students);
+        this.map.clear();
+        this.map.putAll(students);
     }
 
-    public ArrayList<Student> getAll(){
+    public ArrayList<Student> getAll() {
+        ArrayList<Student> students = new ArrayList<>();
+        for (Student student:map.values()){
+            students.add(student);
+        }
         return students;
     }
 
     public Student getById(long id) {
-        for (Student student : students) {
-            if (student.getId() == id) {
-                return student;
-            }
-        }
-        return null;
+        return map.get(id);
     }
 
     public ArrayList<Student> findByName(String firstName) {
         ArrayList<Student> result = new ArrayList<>();
-        for (Student student : students) {
+        for (Student student : map.values()) {
             if (student.getFirstName().equals(firstName)) {
                 result.add(student);
             }
@@ -64,7 +64,7 @@ public class StudentController {
 
     public ArrayList<Student> findByClassName(String className) {
         ArrayList<Student> result = new ArrayList<>();
-        for (Student student : students) {
+        for (Student student : map.values()) {
             if (student.getClassName().equals(className)) {
                 result.add(student);
             }
--- src/test/java/StudentPatternBuilderTest.java
+++ src/test/java/StudentPatternBuilderTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class StudentPatternBuilderTest {
@@ -9,25 +9,24 @@ public class StudentPatternBuilderTest {
     private final String path = "src/main/resources/students.json";
 
     @Test
-    public void test_addStudent() throws IOException {
-        Calendar birthday = Calendar.getInstance();
-        birthday.set(1999, 1, 21);
-        Student a = new Student.StudentBuilder()
-                .setId(5)
-                .setFirstName("Kim")
-                .setLastName("Chon")
-                .setBirthday(birthday)
-                .setClassName("Class 5")
-                .setAddress("Hoa Minh - Lien Chieu")
-                .build();
-        studentController.add(a);
-        studentController.saveToFile(path);
-    }
-    @Test
-    public void test_getFromFile() throws FileNotFoundException {
+    public void test_studentController() throws IOException {
         studentController.loadFromFile(path);
-        for (Student student:studentController.getAll()){
-            System.out.println(student.getBirthday());
-        }
+        Calendar birthday = Calendar.getInstance();
+        birthday.set(1999, 5, 20);
+        Student student = new Student.StudentBuilder()
+                .setId(10)
+                .setFirstName("abc")
+                .setLastName("xyz")
+                .setBirthday(birthday)
+                .setAddress("Hue Viet Nam")
+                .build();
+        studentController.add(student);
+        studentController.delete(student);
+        studentController.deleteAt(1);
+        ArrayList<Student> students = studentController.getAll();
+        ArrayList<Student> students2 = studentController.findByName("kim");
+        ArrayList<Student> students3 = studentController.findByClassName("Class 5");
+        Student student1=studentController.getById(2);
+        studentController.saveToFile(path);
     }
 }
