805b11c90663b74da549f5ef6b6c144ccaa90917
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 14:28:00 +0700

    Fix

--- src/main/java/StackNumber.java
+++ src/main/java/StackNumber.java
@@ -1,21 +1,34 @@
-import java.util.Stack;
+import java.util.ArrayList;
 
 public class StackNumber {
-    Stack<Number> numberStack = new Stack<>();
-    public void push(Number item){
-        numberStack.push(item);
-    }
-    public Number pop(){
-        return numberStack.pop();
-    }
-    public Number peek(){
-        return numberStack.peek();
-    }
-    public int search(Number item){
-        return numberStack.search(item);
-    }
-    public boolean empty(){
-        return numberStack.empty();
+    private ArrayList<Number> numberStack = new ArrayList<>();
+
+    public void push(Number e) {
+        numberStack.add(e);
     }
 
+    public Number pop() {
+        Number num = numberStack.get(numberStack.size() - 1);
+        numberStack.remove(numberStack.size() - 1);
+        return num;
+    }
+
+    public Number peek() {
+        return numberStack.get(numberStack.size() - 1);
+    }
+
+    public int search(Number e) {
+        if (numberStack.lastIndexOf(e) >= 0) {
+            return numberStack.size() - numberStack.lastIndexOf(e) - 1;
+        }
+        return -1;
+    }
+
+    public boolean empty() {
+        return numberStack.isEmpty();
+    }
+
+    public ArrayList<Number> getAll() {
+        return numberStack;
+    }
 }
--- src/test/java/StackNumberTest.java
+++ src/test/java/StackNumberTest.java
@@ -1,16 +1,27 @@
 import org.junit.jupiter.api.Test;
 
-public class StackNumberTest {
-    StackNumber stackNumber = new StackNumber();
-    @Test
-    public void test_stackNumber(){
-        stackNumber.push(5);
-        stackNumber.push(6);
-        stackNumber.push(9);
-        stackNumber.pop();
-        stackNumber.peek();
-        stackNumber.search(5);
-        stackNumber.empty();
-    }
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackNumberTest {
+    private StackNumber stackNumber = new StackNumber();
+
+    @Test
+    public void test_stackNumber() {
+        stackNumber.push(6);
+        assertEquals(stackNumber.getAll(), new ArrayList<>(Arrays.asList(6)));
+        stackNumber.push(9);
+        assertEquals(stackNumber.getAll(), new ArrayList<>(Arrays.asList(6, 9)));
+        assertEquals(stackNumber.peek(), 9);
+        assertEquals(stackNumber.getAll(), new ArrayList<>(Arrays.asList(6, 9)));
+        assertEquals(stackNumber.pop(), 9);
+        assertEquals(stackNumber.getAll(), new ArrayList<>(Arrays.asList(6)));
+        stackNumber.push(3);
+        stackNumber.push(12);
+        assertEquals(stackNumber.search(0), -1);
+        assertEquals(stackNumber.search(3), 1);
+        assertEquals(stackNumber.empty(), false);
+    }
 }
