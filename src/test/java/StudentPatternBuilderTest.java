45268ee7f02fa0d53533d8948b64a50e232c4e04
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 17:33:16 +0700

    Add test case

--- src/test/java/StudentPatternBuilderTest.java
+++ src/test/java/StudentPatternBuilderTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StudentPatternBuilderTest {
     private StudentController studentController = new StudentController();
     private final String root_path = "src/main/resources/students.json";
@@ -24,8 +26,11 @@ public class StudentPatternBuilderTest {
                 .build();
         studentController.add(student);
         ArrayList<Student> students = studentController.getAll();
+        assertEquals(students.get(0).getAddress(),"Hoa Minh - Lien Chieu");
         ArrayList<Student> students2 = studentController.findByName("Quang");
+        assertEquals(students.get(0).getLastName(),"Vo");
         ArrayList<Student> students3 = studentController.findByClassName("Class5");
+        assertEquals(students3.get(0).getBirthday().get(Calendar.YEAR),1999);
         Student student1 = studentController.getById(2);
         studentController.delete(student);
         studentController.deleteAt(1);
