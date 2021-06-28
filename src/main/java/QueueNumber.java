5751d8af99c9d89eead3ff83af5e88339dab239b
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 15:28:17 +0700

    Fix

--- src/main/java/QueueNumber.java
+++ src/main/java/QueueNumber.java
@@ -1,7 +1,62 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class QueueNumber {
     private ArrayList<Number> numberQueue = new ArrayList<>();
+    private final int size = 5;
+
+    public int size() {
+        return numberQueue.size();
+    }
+
+    public boolean add(Number e) throws Exception {
+        if (size() == size) {
+            throw new Exception("Stack over flow");
+        }
+        numberQueue.add(e);
+        return true;
+    }
+
+    public boolean offer(Number e) {
+        if (size() == size) {
+            return false;
+        }
+        numberQueue.add(e);
+        return true;
+    }
+
+    public Number element() throws Exception {
+        if (size() == 0) {
+            throw new Exception("Queue is empty");
+        }
+        return numberQueue.get(0);
+    }
+
+    public Number peek() {
+        if (size() == 0) {
+            return null;
+        }
+        return numberQueue.get(0);
+    }
+
+    public Number remove() throws Exception {
+        if (size() == 0) {
+            throw new Exception("Queue is empty");
+        }
+        Number number = numberQueue.get(0);
+        numberQueue.remove(0);
+        return number;
+    }
+
+    public Number poll() {
+        if (size() == 0) {
+            return null;
+        }
+        Number number = numberQueue.get(0);
+        numberQueue.remove(0);
+        return number;
+    }
+
+    public ArrayList<Number> getAll() {
+        return numberQueue;
+    }
 }
--- src/test/java/QueueNumberTest.java
+++ src/test/java/QueueNumberTest.java
@@ -1,16 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class QueueNumberTest {
     QueueNumber queueNumber = new QueueNumber();
 
     @Test
-    public void test_queueNumber() {
-        queueNumber.add(1);
-        queueNumber.offer(3);
-        queueNumber.element();
-        queueNumber.peek();
+    public void test_queueNumber() throws Exception {
+        assertEquals(queueNumber.peek(),null);
+        assertEquals(queueNumber.poll(),null);
+        queueNumber.add(3);
+        queueNumber.add(6);
+        queueNumber.add(10);
+        assertEquals(queueNumber.getAll(), new ArrayList<>(Arrays.asList(3, 6, 10)));
+        queueNumber.offer(13);
+        assertEquals(queueNumber.getAll(), new ArrayList<>(Arrays.asList(3, 6, 10, 13)));
+        assertEquals(queueNumber.element(),3);
+        assertEquals(queueNumber.offer(6),true);
+        assertEquals(queueNumber.peek(),3);
+        assertEquals(queueNumber.remove(),3);
         queueNumber.poll();
-        queueNumber.remove();
-
     }
 }
