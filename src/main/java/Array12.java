b0fb00df070953ea433e9086eb3607afa859689a
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 11:53:46 +0700

    Fix

--- src/main/java/Array12.java
+++ src/main/java/Array12.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+
 public class Array12 {
     public boolean arrayEquals(int[] a, int[] b) {
-        int lenA = a.length;
-        int lenB = b.length;
-        if (lenA != lenB) {
+        if (a.length != b.length) {
             return false;
         }
-        for (int i = 0; i < lenA; i++) {
+        ArrayList<Integer> check = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
             if (a[i] != b[i]) {
-                return false;
+                check.add(i);
             }
         }
-        return true;
+        if (check.size() == 1 || check.size() > 2) {
+            return false;
+        }
+        if (check.size() == 0) {
+            return true;
+        }
+        return a[check.get(0)] == b[check.get(1)] && a[check.get(1)] == b[check.get(0)];
     }
 }
--- src/test/java/Array12Test.java
+++ src/test/java/Array12Test.java
@@ -5,12 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Array12Test {
     @Test
     public void test_arrayEquals() {
-        int[] a1 = {1, 2, 3};
-        int[] a2 = {1, 2, 3};
-        int[] a3 = {1, 2, 3, 4};
-        int[] a4 = {2, 3, 4};
-        assertEquals(new Array12().arrayEquals(a1, a2), true);
+        int[] a1 = {1, 2, 3, 6, 8};
+        int[] a2 = {1, 2, 3, 5};
+        int[] a3 = {16, 3, 12, 6, 8};
+        int[] a4 = {3, 2, 3, 7, 8};
+        int[] a5 = {3, 2, 3, 7, 8};
+        int[] a6 = {3, 7, 3, 2, 8};
+        assertEquals(new Array12().arrayEquals(a1, a2), false);
         assertEquals(new Array12().arrayEquals(a1, a3), false);
         assertEquals(new Array12().arrayEquals(a1, a4), false);
+        assertEquals(new Array12().arrayEquals(a5, a4), true);
+        assertEquals(new Array12().arrayEquals(a6, a4), true);
     }
 }
