025c529111f25bdb84bdff63cdeb2cdd04157eed
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 18:51:45 +0700

    fix

--- src/test/java/StackGenericTest.java
+++ src/test/java/StackGenericTest.java
@@ -6,18 +6,18 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StackGenericTest {
-    private StackGeneric stackGeneric = new StackGeneric();
+    private StackGeneric<String> stackGeneric = new StackGeneric<>();
 
     @Test
     public void test_stackGeneric() {
         stackGeneric.push("p");
         assertEquals(stackGeneric.peek(), "p");
-        stackGeneric.push(1);
-        stackGeneric.push('t');
-        assertEquals(stackGeneric.pop(), 't');
-        assertEquals(stackGeneric.getAll(), new ArrayList<>(Arrays.asList("p", 1)));
-        assertEquals(stackGeneric.empty(),false);
-        assertEquals(stackGeneric.search("p"),1);
-        assertEquals(stackGeneric.search("pa"),-1);
+        stackGeneric.push("9");
+        stackGeneric.push("t");
+        assertEquals(stackGeneric.pop(), "t");
+        assertEquals(stackGeneric.getAll(), new ArrayList<>(Arrays.asList("p", "9")));
+        assertEquals(stackGeneric.empty(), false);
+        assertEquals(stackGeneric.search("p"), 1);
+        assertEquals(stackGeneric.search("pa"), -1);
     }
 }
