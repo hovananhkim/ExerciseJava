8dd1d2bc64b4b15f7d0bca7a29dac35f3d1e2b67
hovananhkim <hvakim.vn@gmail.com>
2021-06-28 10:49:25 +0700

    Format code

--- src/main/java/Array15.java
+++ src/main/java/Array15.java
@@ -4,6 +4,6 @@ public class Array15 implements MaxArray {
     @Override
     public int[] findMaxArray(int[] array, int k) {
         Arrays.sort(array);
-        return Arrays.copyOfRange(array,array.length-k,array.length);
+        return Arrays.copyOfRange(array, array.length - k, array.length);
     }
 }
