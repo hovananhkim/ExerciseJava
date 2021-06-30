d749cfbbfbabcbc8d30d00d9239caeb3752c84f7
hovananhkim <hvakim.vn@gmail.com>
2021-06-30 10:43:59 +0700

    fix

--- src/main/java/OOP/QueueNumber.java
+++ src/main/java/OOP/QueueNumber.java
@@ -4,61 +4,21 @@ import java.util.ArrayList;
 
 public class QueueNumber {
     private ArrayList<Number> numberQueue = new ArrayList<>();
-    private final int size = 5;
 
-    public int size() {
-        return numberQueue.size();
-    }
-
-    public boolean add(Number e) throws Exception {
-        if (size() == size) {
-            throw new Exception("Stack over flow");
-        }
+    public void enqueue(Number e) {
         numberQueue.add(e);
-        return true;
     }
 
-    public boolean offer(Number e) {
-        if (size() == size) {
-            return false;
-        }
-        numberQueue.add(e);
-        return true;
-    }
-
-    public Number element() throws Exception {
-        if (size() == 0) {
+    public Number dequeue() throws Exception {
+        if (isEmpty()) {
             throw new Exception("Queue is empty");
         }
-        return numberQueue.get(0);
-    }
-
-    public Number peek() {
-        if (size() == 0) {
-            return null;
-        }
-        return numberQueue.get(0);
-    }
-
-    public Number remove() throws Exception {
-        if (size() == 0) {
-            throw new Exception("Queue is empty");
-        }
-        Number number = numberQueue.get(0);
+        Number result = numberQueue.get(0);
         numberQueue.remove(0);
-        return number;
+        return result;
     }
 
-    public Number poll() {
-        if (size() == 0) {
-            return null;
-        }
-        Number number = numberQueue.get(0);
-        numberQueue.remove(0);
-        return number;
-    }
-
-    public ArrayList<Number> getAll() {
-        return numberQueue;
+    public boolean isEmpty() {
+        return numberQueue.isEmpty();
     }
 }
--- src/test/java/OOPTest/QueueNumberTest.java
+++ src/test/java/OOPTest/QueueNumberTest.java
@@ -1,6 +1,7 @@
 package OOPTest;
 
 import OOP.QueueNumber;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,34 +10,26 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class QueueNumberTest {
-    QueueNumber queueNumber = new QueueNumber();
+    public static QueueNumber queueNumber = new QueueNumber();
+    
+    @BeforeAll
+    public static void init() {
+        queueNumber.enqueue(1);
+        queueNumber.enqueue(12);
+        queueNumber.enqueue(16);
+    }
 
     @Test
-    public void test_queueNumber() throws Exception {
-        assertEquals(queueNumber.peek(), null);
+    public void test_dequeue() throws Exception {
+        assertEquals(queueNumber.dequeue(), 1);
+        assertEquals(12, queueNumber.dequeue());
+        assertEquals(16, queueNumber.dequeue());
         assertThrows(Exception.class, () -> {
-            queueNumber.element();
+            queueNumber.dequeue();
         });
-        assertThrows(Exception.class, () -> {
-            queueNumber.remove();
-        });
-        assertEquals(queueNumber.poll(), null);
-        queueNumber.add(3);
-        queueNumber.add(6);
-        queueNumber.add(10);
-        assertEquals(queueNumber.getAll(), new ArrayList<>(Arrays.asList(3, 6, 10)));
-        queueNumber.offer(13);
-        queueNumber.add(6);
-        assertThrows(Exception.class, () -> {
-            queueNumber.add(5);
-        });
-        assertEquals(queueNumber.getAll(), new ArrayList<>(Arrays.asList(3, 6, 10, 13, 6)));
-        assertEquals(queueNumber.element(), 3);
-
-        assertEquals(queueNumber.offer(6), false);
-        assertEquals(queueNumber.peek(), 3);
-        assertEquals(queueNumber.remove(), 3);
-        queueNumber.poll();
-        assertEquals(queueNumber.getAll(), new ArrayList<>(Arrays.asList(10, 13, 6)));
+    }
+    @Test
+    public void test_isEmpty(){
+        assertTrue(queueNumber.isEmpty());
     }
 }
